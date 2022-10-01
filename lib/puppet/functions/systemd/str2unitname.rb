48ee0ce494f5c52d465d8484937e7386bcc7ea3d
Mat Philip <mvphilip@gmail.com>
2022-10-01 14:30:57 -0700

    Update str2unitname.rb
    Using systemd-escape

--- lib/puppet/functions/systemd/str2unitname.rb
+++ lib/puppet/functions/systemd/str2unitname.rb
@@ -10,21 +10,18 @@ Puppet::Functions.create_function(:'systemd::str2unitname') do
   end
 
   def unitname(s, pathmode=true)
-    t = s
 
-    # In path mode, drop leading and trailing slashes, then collapse duplicates
-    t = t.gsub(/\/\/+/,'/').gsub(/\/$/,'').gsub(/^\//,'') if (pathmode)
-
-    # In path mode, if the result is now empty, we'll make
-    # it represent the root directory
-    return '-' if pathmode && t.empty?
-
-    # \x<nn> escape all chars except alpha-numerics, underbars, slashes and dots
-    t = t.gsub(/([^A-Za-z0-9_\/.])/) { |c| "\\x#{c.ord.to_s(16)}" }
-
-    # Replace all remaining slashes with hyphens
-    # and escape any remaining leading dot
-    return t.gsub(/\//,'-').gsub(/^\./, '\\x2e')
+    #input = s[/.(.*)/m,1]
+    input = s
+    if system('which systemd-escape') == true
+      output = `systemd-escape -p --suffix=mount #{input}`
+    else
+      input = input.gsub(/-/,'\\x2d')
+      input = input.gsub(/\//,'-')
+      output = input
+    end
+    notify {"SYSTEMD-OUT : ${output}" }
+    return output.chomp
   end
 
 end
